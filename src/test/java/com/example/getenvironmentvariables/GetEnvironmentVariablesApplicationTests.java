bde21c4bce15bd68383991aef44f6a9d817aa11a
jarltuxen <jarl.tuxen@gmail.com>
2022-03-29 15:10:59 +0200

    Initial commit

---
+++ src/test/java/com/example/getenvironmentvariables/GetEnvironmentVariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getenvironmentvariables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetEnvironmentVariablesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
